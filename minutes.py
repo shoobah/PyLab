307e6640130f9db5bcbc9669fc0cea5e4567fbdd
Anders Sjöberg <shoobah@gmail.com>
2022-12-05 23:54:00 +0100

    Current time option

--- minutes.py
+++ minutes.py
@@ -12,14 +12,16 @@ datetime1 = datetime.datetime.strptime(date1, "%Y-%m-%d %H:%M")
 current_time = datetime.datetime.now()
 
 # Prompt the user to select the current time or enter a custom time
-choice = input("Select the current time (enter 'c') or enter a custom time (enter 'custom'): ")
+choice = input(
+    "Select the current time (enter 'c') or enter a custom time (enter 'custom'): ")
 
 if choice == "c":
     # Use the current time as the second date-time
     datetime2 = current_time
 else:
     # Prompt the user to enter a custom time
-    date2 = input("Enter the second date-time (in the format YYYY-MM-DD HH:MM): ")
+    date2 = input(
+        "Enter the second date-time (in the format YYYY-MM-DD HH:MM): ")
 
     # Convert the second date-time to a datetime object
     datetime2 = datetime.datetime.strptime(date2, "%Y-%m-%d %H:%M")
